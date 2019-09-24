98be860c6dec91eed46c56ddac4f7a0158ac4a09
GriffinJ <jrgriffiniii@gmail.com>
2019-09-24 14:17:13 -0400

    WIP

--- src/containers/AuthButton.js
+++ src/containers/AuthButton.js
@@ -5,13 +5,15 @@ import PropTypes from 'prop-types';
 class AuthButton extends React.Component {
   render() {
     return (
-      <Button variant="contained" color="secondary" style={this.props.style}>Sign In</Button>
+      <Button variant="contained" color="secondary" style={this.props.style} onClick={this.props.handleClick} href={this.props.authorizationURL}>Sign In</Button>
     );
   }
 }
 
 AuthButton.propTypes = {
-  style: PropTypes.object
+  style: PropTypes.object,
+  handleClick: PropTypes.func.isRequired,
+  authorizationURL: PropTypes.string.isRequired
 };
 
 export default AuthButton;
