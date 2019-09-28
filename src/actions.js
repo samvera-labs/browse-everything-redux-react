17a2d02a94cc088ed3bdd71d1b06e1e1cb32d715
GriffinJ <jrgriffiniii@gmail.com>
2019-09-28 15:47:04 -0400

    Containers now render

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,9 @@
 import { selectProvider, updateProviders } from './actions/providers';
 import { createSession } from './actions/sessions';
 import { authorize } from './actions/authorizations';
-import { getRootContainer, updateRootContainer } from './actions/containers';
+import { getRootContainer } from './actions/containers';
 
 export { selectProvider, updateProviders };
 export { createSession };
 export { authorize };
-export { getRootContainer, updateRootContainer };
+export { getRootContainer };
